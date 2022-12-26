23bd4e5ed9d5d2648ab0af2316282bf0981d3fb8
Jonathan Metz <jcmetz21@gmail.com>
2022-12-26 12:13:57 -0800

    Better org

--- code-examples/dj-scratch/scripts/debugger.py
+++ code-examples/dj-scratch/scripts/debugger.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 step = 500_000
-total = 1_000_000
+total = 5_000_000
 Todo.objects.all().delete()
 call_command("migrate", "customsort")
 with timer(logger, name=f"inserted {total:,} objects"):
@@ -29,64 +29,51 @@ with timer(logger, name=f"inserted {total:,} objects"):
 count = Todo.objects.count()
 assert count == total, f"{count:,} != {total:,}"
 
-# with timer(logger, name="order in DB with integer choices", decimals=1):
-#     qs = Todo.objects.order_by("priority", "title")
-#     logger.debug(qs.explain())
-#     fetch(qs)
+with timer(logger, name="order in Python", decimals=1):
+    preference = {
+        Todo.Priority.HIGH: 1,
+        Todo.Priority.MEDIUM: 2,
+        Todo.Priority.LOW: 3,
+    }
+    sorted(
+        Todo.objects.all(),
+        key=lambda x: [preference[x.priority], x.title],
+    )
 
-# with timer(logger, name="order in DB with conditional expression", decimals=1):
-#     qs = Todo.objects.order_by_priority()
-#     logger.debug(qs.explain())
-#     fetch(qs)
+call_command("migrate", "customsort")
+with timer(logger, name="order in DB with integer choices", decimals=1):
+    qs = Todo.objects.order_by("priority", "title")
+    logger.debug(qs.explain())
+    fetch(qs)
+with timer(logger, name="order in DB with conditional expression", decimals=1):
+    qs = Todo.objects.order_by_priority()
+    logger.debug(qs.explain())
+    fetch(qs)
 
-# with connection.cursor() as cursor:
-#     cursor.execute(
-#         "SELECT indexname FROM pg_indexes WHERE tablename='customsort_todo' ORDER BY tablename, indexname"
-#     )
-#     rows = cursor.fetchall()
-#     print(rows)
-# call_command("migrate", "customsort", "0001")
-# with connection.cursor() as cursor:
-#     cursor.execute(
-#         "SELECT indexname FROM pg_indexes WHERE tablename='customsort_todo' ORDER BY tablename, indexname"
-#     )
-#     rows = cursor.fetchall()
-#     print(rows)
-#
-# with timer(logger, name="order in DB with integer choices w/o index", decimals=1):
-#     qs = Todo.objects.order_by("priority", "title")
-#     logger.debug(qs.explain())
-#     fetch(qs)
-#
-# with timer(
-#     logger, name="order in DB with conditional expression w/o index", decimals=1
-# ):
-#     qs = Todo.objects.order_by_priority()
-#     logger.debug(qs.explain())
-#     fetch(qs)
+with connection.cursor() as cursor:
+    cursor.execute(
+        "SELECT indexname FROM pg_indexes WHERE tablename='customsort_todo' ORDER BY tablename, indexname"
+    )
+    rows = cursor.fetchall()
+    print(rows)
+call_command("migrate", "customsort", "0001")
+with connection.cursor() as cursor:
+    cursor.execute(
+        "SELECT indexname FROM pg_indexes WHERE tablename='customsort_todo' ORDER BY tablename, indexname"
+    )
+    rows = cursor.fetchall()
+    print(rows)
 
-# with timer(logger, name="order in Python", decimals=1):
-#     preference = {
-#         Todo.Priority.HIGH: 1,
-#         Todo.Priority.MEDIUM: 2,
-#         Todo.Priority.LOW: 3,
-#     }
-#     sorted(
-#         Todo.objects.all(),
-#         key=lambda x: [preference[x.priority], x.title],
-#     )
-
-
-# PRIORITY_ORDER = {
-#     Todo.Priority.HIGH: 1,
-#     Todo.Priority.MEDIUM: 2,
-#     Todo.Priority.LOW: 3,
-# }
-# with timer("get todos", decimals=1):
-#     todos = list(Todo.objects.all())
-# with timer("sort in Python", decimals=1):
-#     # todos = sorted(todos, key=lambda x: [PRIORITY_ORDER[x.priority], x.title])
-#     todos.sort(key=lambda x: [PRIORITY_ORDER[x.priority], x.title])
+with timer(logger, name="order in DB with integer choices w/o index", decimals=1):
+    qs = Todo.objects.order_by("priority", "title")
+    logger.debug(qs.explain())
+    fetch(qs)
+with timer(
+    logger, name="order in DB with conditional expression w/o index", decimals=1
+):
+    qs = Todo.objects.order_by_priority()
+    logger.debug(qs.explain())
+    fetch(qs)
 
 
 def run():
